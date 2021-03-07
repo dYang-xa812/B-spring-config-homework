7c0ade84a84036e9844996bf1d11d2ed8c80c265
D Yang <moxuan812@foxmail.com>
2021-03-07 13:33:59 +0800

    [Yang Di] feat: p2s2 submit

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationEqualTest.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationEqualTest.java
@@ -7,22 +7,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(properties = "levelNumber = 0")
-class DemoSpringConfigApplicationTests {
-	@Autowired
-	MockMvc mockMvc;
+@TestPropertySource(properties = "levelNumber = 1")
+public class DemoSpringConfigApplicationEqualTest {
+    @Autowired
+    MockMvc mockMvc;
 
-	@Autowired
-	LevelController levelController;
+    @Autowired
+    LevelController levelController;
 
-	@Test
-	void should_return_basic_when_levelNumber_is_less_than_1() throws Exception {
-		mockMvc.perform(get("/level"))
-				.andExpect(MockMvcResultMatchers.content().string("basic"));
-	}
-
+    @Test
+    void should_return_advanced_when_levelNumber_equals_to_1() throws Exception {
+        mockMvc.perform(get("/level"))
+                .andExpect(MockMvcResultMatchers.content().string("advanced"));
+    }
 }
